24de52f1d260ab575c5bbdd996643c32f3241940
djsavvy <github.com/djsavvy>
2018-12-02 14:57:11 -0500

    Test KPMatrix implementation

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@
 
 int main() {
 
-    std::cout << "Hello world" << std::endl;
+    KPMatrix test_matr(5, 5);
+    test_matr.set(0, 2, -3);
+    test_matr.set(3, 4, 5);
+    test_matr.set(4, 0, -1);
 
+    std::cout << test_matr.get(0, 2) << std::endl;
+    std::cout << test_matr.get(3, 4) << std::endl;
+    std::cout << test_matr.squared_frobenius_norm() << std::endl;
+    std::cout << test_matr.sample_a_row() << std::endl;
+    std::cout << test_matr.sample_from_row(0) << std::endl;
+    std::cout << test_matr.sample_from_row(2) << std::endl;
+
+
+
+    /*
     KPVector test_vec(10);
     test_vec.set(0, 2);
     test_vec.set(6, 7);
@@ -21,12 +34,13 @@ int main() {
     std::cout << "----" << std::endl;
 
     for(int i = 0; i < 10; ++i) {
-        std::cout << "Sample value: " << test_vec.sample_value() << std::endl;
+        std::cout << "Sample value: " << test_vec.get(test_vec.sample_index()) << std::endl;
         std::cout << "Sample index: " << test_vec.sample_index() << std::endl;
     }
 
     std::cout << "----" << std::endl;
 
     std::cout << test_vec << std::endl;
+    */
 
 }
